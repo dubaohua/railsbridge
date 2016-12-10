604d4dc8f75612f259071f9c3d66074ab93cbbc2
dubaohua <dubaohua.ying@gmail.com>
2016-12-10 22:01:59 +0800

    routes.rb& merge upvote and downvote

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,12 @@ Rails.application.routes.draw do
   resources :topics do
     member do
       post 'upvote'
-    end
-  end
-
-
-  resources :topics do
-    member do
       post 'downvote'
     end
   end
+
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
